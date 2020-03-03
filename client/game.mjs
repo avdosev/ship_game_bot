5b4c37587a9d68673565b23cc914271cc7bc9735
avdosev <avdosev1@yandex.ru>
2020-03-03 21:23:42 +0400

    skeleton bot

--- client/game.mjs
+++ client/game.mjs
@@ -1,7 +1,41 @@
+const MAX_LOAD_SHIP = 368;
+
 export function startGame(levelMap, gameState) {
 
 }
 
+
 export function getNextCommand(gameState) {
-    return 'WAIT';
+    const homePort = getHomePort(gameState)
+    const ship = gameState.ship;
+    const shipOnHome = isEqualPosition(ship, homePort)
+    if (shipOnHome && canLoadProduct(gameState)) {
+        // нужно загрузить максимум по максимальной цене
+
+    } else if (onTradingPort(gameState)) {
+
+    } else if (shipOnHome && !canLoadProduct(gameState)) { // уже загрузили товар
+        // перемещаемся к цели
+    }
+    return 'WAIT'
+}
+
+//орррророоророороорооооорооорррррооорр
+function canLoadProduct(gameState) {
+    return gameState.ship.goods.length == 0;
+}
+
+function onTradingPort(gameState) {
+    const { ship, ports } = gameState;
+    return ports.reduce((acc, port) => acc || (isEqualPosition(port, ship) && !port.isHome), false);
+}
+
+function isEqualPosition(obj1, obj2) {
+    return obj1.x == obj2.x && obj1.y == obj2.y;
+}
+
+function getHomePort(gameState) {
+    const ship = gameState.ship;
+    const portsOnCurrentPosition = gameState.ports.filter(port => ship.x == port.x && ship.y == port.y)
+    return portsOnCurrentPosition[0];
 }
